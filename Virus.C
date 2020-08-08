82d91faf6889759e35e002915d542d80579ac3a0
Venom3722 <69351649+Venom3722@users.noreply.github.com>
2020-08-08 11:19:05 +0530

    Update Virus.C

--- Virus.C
+++ Virus.C
@@ -5,4 +5,4 @@ int main(void)
     fork(); 
   return 0; 
 }
-/* I am not responsible and will not accept any alligation thrown to my respriotry, Compile at your own risk and again i say, I AM NOT RESPONSIBLE, NEITHER WILL BE, FOR ANY DAMAGE CAUSED BY YOU TO OTHERS*/
+//This code will create an infinite loop that will freeze your device...
